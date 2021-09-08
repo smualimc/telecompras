01bb1dede6faef5de2df7d430c143de92f161de0
smualimc <smualim@hotmail.com>
2021-09-08 08:16:01 -0300

    style cart button in store.scss, has_many line in models Cart & Product, add button store/index.html.erb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+
     validates :title, :description, :image_url, :price, presence: true 
     validates :price, numericality: { greater_than_or_equal_to: 10 }
     validates :title, uniqueness: true
@@ -6,4 +10,13 @@ class Product < ApplicationRecord
         with: /\w+\.(jpg|png)\z/i,
         message: "debe ser una imágen JPG o PNG "
       }
+
+      private
+
+      def ensure_not_referenced_by_any_line_item
+        unless line.items.empty?
+          errors.add(:base, 'Line Items present')
+            throw :abort
+        end
+      end 
 end
