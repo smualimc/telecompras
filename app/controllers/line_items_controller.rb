8f42801ad6d8797ff39588a8d67a162a7cbd2f70
smualimc <smualim@hotmail.com>
2021-09-13 17:22:47 -0300

    show cart as a table with totals by item & cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: "Ítem agregado con éxito" }
+        format.html { redirect_to @line_item.cart}
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
